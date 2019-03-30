896df6f044b3d6d88b63829debfc910029654770
Holben888 <holben888@gmail.com>
2019-03-30 07:34:45 -0400

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+import svelte from "rollup-plugin-svelte";
+import resolve from "rollup-plugin-node-resolve";
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
+
+const build = {
+  input: "main.js",
+  output: {
+    file: "public/build/bundle.js",
+    name: "App",
+    format: "iife"
+  },
+  plugins: [
+    resolve(),
+    svelte({
+      css: stylesheet => {
+        stylesheet.write("public/build/bundle.css");
+      }
+    })
+  ]
+};
+
+if (process.env.BUILD === "development")
+  build.plugins = [
+    ...build.plugins,
+    serve({
+      open: true,
+      contentBase: "public",
+      port: 5000
+    }),
+    livereload({
+      watch: "public/build"
+    })
+  ];
+
+export default build;
